2f84fac9bc725e88fd47740571cc147ce0e22325
razan0010 <132309368+razan0010@users.noreply.github.com>
2023-08-20 19:53:34 +0300

    Add files via upload

---
+++ schoolmanagement/Repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.example.schoolmanagement.Repository;
+
+import com.example.schoolmanagement.Model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address , Integer> {
+}
